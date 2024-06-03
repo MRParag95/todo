6261c5abb50df714d4ea3763619c205df4addcd5
MRParag95 <rahman.parag.95@gmail.com>
2024-06-03 17:45:03 +0600

    Updated Exception Handling.

--- src/main/java/com/mendax47/todo/config/GlobalExceptionHandlerConfig.java
+++ src/main/java/com/mendax47/todo/config/GlobalExceptionHandlerConfig.java
@@ -1,7 +1,16 @@
 package com.mendax47.todo.config;
 
+import com.mendax47.todo.exceptions.ResourceNotFound;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 
 @RestControllerAdvice
 public class GlobalExceptionHandlerConfig {
+    @ExceptionHandler(ResourceNotFound.class)
+    public ResponseEntity<String> resourceNotFound(ResourceNotFound exception, WebRequest request) {
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
+    }
 }
